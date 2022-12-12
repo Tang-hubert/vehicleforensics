7d206e3b0e839440235f00e475fcbb97364d852e
Tang-hubert <hubert112247@gmail.com>
2022-12-12 21:50:09 +0800

    chore: add some notes.

--- evaluation/falcon/ntru_config.py
+++ evaluation/falcon/ntru_config.py
@@ -1,10 +1,19 @@
+# Primes used in Key Generation
 N_P = 3
-N_Q = 128
 N_D = 5
-N_elliptic_a = 5
-N_elliptic_b = 7
 N_N = 13
 
+# Integer numbers used in Key Generation
+N_Q = 128
+
+# [Elliptic Curve Cryptography, ECC] defense againest brute force
+N_elliptic_a = 5
+N_elliptic_b = 7
+
+
+# Parameters from secret key
 N_F = [1, -1, 0, 0, 0, -1, 0, 0, 1, 0, 1, 0, 0]
 N_g = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, -1, 0]
+
+# Parameter from public key
 N_h = [22, -39, -35, 62, 14, 24, 62, -19, 42, 32, -56, -20, -18, -62]
